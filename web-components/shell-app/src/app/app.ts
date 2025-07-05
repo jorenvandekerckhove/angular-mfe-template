ccf94d3f02ebe39c5f44db8eb6e36cc05c1ba2d3
jorenvandekerckhove <vdk.joren@hotmail.com>
2025-07-05 14:21:21 +0200

    Moved the mfe loader component

--- web-components/shell-app/src/app/app.ts
+++ web-components/shell-app/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { MfeLoaderComponent } from './mfe-loader.component';
+import { MfeLoaderComponent } from '../components/mfe-loader/mfe-loader.component';
 
 @Component({
   selector: 'app-root',
   imports: [MfeLoaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  standalone: true
+  standalone: true,
 })
 export class App {
   protected title = 'shell-app';
